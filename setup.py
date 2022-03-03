101d74e51488fc90dbb0ef11a1fddff365fc3a95
Matt Harrison <matt@noloco.io>
2022-03-03 17:06:40 +0000

    chore: scaffolds project (#1)
    * Sets up and documents pip3 env
    
    * Adds empty module scaffolding
    
    * Scaffolds documentation generator
    
    * Improves README
    
    * Ignores .DS_Store files
    
    * Ignores docs from root package
    
    * New line at EOF

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='Noloco',
+    version='0.1.0',
+    description='Noloco Python SDK',
+    long_description='', # TODO - write downstream-friendly README
+    author='Noloco',
+    author_email='team@noloco.io',
+    url='https://github.com/noloco/python-sdk',
+    license='', # TODO - write LICENSE
+    packages=find_packages(exclude=('docs', 'tests'))
+)

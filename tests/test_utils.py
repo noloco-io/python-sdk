823818ca38f44a2c44ac7da0ae15bc26aef4b2f6
Matt Harrison <matt@noloco.io>
2022-03-07 23:17:50 +0000

    Adds more test coverage to utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,7 +2,9 @@ from noloco.utils import (
     build_data_type_arg,
     build_data_type_args,
     build_operation_arg,
-    build_operation_args)
+    build_operation_args,
+    gql_args,
+    has_files)
 from unittest import TestCase
 
 
@@ -75,3 +77,71 @@ class TestBuildOperationArgs(TestCase):
         built_args = build_operation_args(args)
 
         self.assertEqual('($a: String, $c: Int)', built_args)
+
+
+class TestGqlArgs(TestCase):
+    def test_gql_args_builds_no_args(self):
+        args = {}
+
+        built_args = gql_args(args)
+
+        self.assertEqual({}, built_args)
+
+    def test_gql_args_builds_single_arg(self):
+        args = {}
+        args['a'] = {'type': 'String', 'value': 'b'}
+
+        built_args = gql_args(args)
+
+        self.assertEqual({'a': 'b'}, built_args)
+
+    def test_gql_args_builds_multiple_args(self):
+        args = {}
+        args['a'] = {'type': 'String', 'value': 'b'}
+        args['c'] = {'type': 'Int', 'value': 1}
+
+        built_args = gql_args(args)
+
+        self.assertEqual({'a': 'b', 'c': 1}, built_args)
+
+
+class TestHasFiles(TestCase):
+    def test_has_files_no_args(self):
+        args = {}
+
+        self.assertFalse(has_files(args))
+
+    def test_has_files_single_file_arg(self):
+        args = {}
+        args['a'] = {'type': 'Upload'}
+
+        self.assertTrue(has_files(args))
+
+    def test_has_files_single_non_file_arg(self):
+        args = {}
+        args['a'] = {'type': 'String'}
+
+        self.assertFalse(has_files(args))
+
+    def test_has_files_multiple_args_with_file(self):
+        args = {}
+        args['a'] = {'type': 'Boolean'}
+        args['b'] = {'type': 'DateTime'}
+        args['c'] = {'type': 'Duration'}
+        args['g'] = {'type': 'Upload'}
+        args['d'] = {'type': 'Float'}
+        args['e'] = {'type': 'Int'}
+        args['f'] = {'type': 'String'}
+
+        self.assertTrue(has_files(args))
+
+    def test_has_files_multiple_args_without_file(self):
+        args = {}
+        args['a'] = {'type': 'Boolean'}
+        args['b'] = {'type': 'DateTime'}
+        args['c'] = {'type': 'Duration'}
+        args['d'] = {'type': 'Float'}
+        args['e'] = {'type': 'Int'}
+        args['f'] = {'type': 'String'}
+
+        self.assertFalse(has_files(args))

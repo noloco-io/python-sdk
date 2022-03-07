087fed1451223316c8f54c9ad3b629a4254103d1
Matt Harrison <matt@noloco.io>
2022-03-07 12:07:49 +0000

    Adds constants definitions (#5)

---
+++ noloco/constants.py
@@ -0,0 +1,41 @@
+###############################################################################
+# Field Types
+###############################################################################
+
+
+TEXT = 'TEXT'
+
+
+DATE = 'DATE'
+
+
+INTEGER = 'INTEGER'
+
+
+DECIMAL = 'DECIMAL'
+
+
+DURATION = 'DURATION'
+
+
+BOOLEAN = 'BOOLEAN'
+
+
+RICH_TEXT = 'RICH_TEXT'
+
+
+SINGLE_OPTION = 'SINGLE_OPTION'
+
+
+###############################################################################
+# Relationship Types
+###############################################################################
+
+
+ONE_TO_ONE = 'ONE_TO_ONE'
+
+
+MANY_TO_ONE = 'MANY_TO_ONE'
+
+
+MANY_TO_MANY = 'MANY_TO_MANY'

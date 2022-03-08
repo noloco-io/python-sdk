7a714417afd6df597d18a3aa8c1febb35c4d0210
Matt Harrison <matt@noloco.io>
2022-03-08 19:03:27 +0000

    Adds test coverage for MutationBuilder.build_data_type_mutation_args (#14)

---
+++ tests/test_mutations.py
@@ -0,0 +1,85 @@
+from noloco.constants import MANY_TO_ONE, ONE_TO_ONE
+from noloco.mutations import MutationBuilder
+from unittest import TestCase
+
+
+class TestBuildDataTypeMutationArgs(TestCase):
+    def setUp(self):
+        self.__mutation_builder = MutationBuilder()
+
+    def test_top_level_field(self):
+        data_type = {
+            'fields': [
+                {'name': 'a', 'type': 'TEXT', 'relationship': None}
+            ]
+        }
+        data_types = [data_type]
+        args = {'a': 'b'}
+
+        mutation_args = self.__mutation_builder.build_data_type_mutation_args(
+            data_type,
+            data_types,
+            args)
+
+        self.assertEqual(
+            {'a': {'type': 'String', 'value': 'b'}},
+            mutation_args)
+
+    def test_forward_relationship_field(self):
+        data_type = {
+            'fields': [
+                {'name': 'a', 'type': 'A', 'relationship': MANY_TO_ONE}
+            ]
+        }
+        data_types = [data_type]
+        args = {'a': {'connect': {'id': 1}}}
+
+        mutation_args = self.__mutation_builder.build_data_type_mutation_args(
+            data_type,
+            data_types,
+            args)
+
+        self.assertEqual(
+            {'aId': {'type': 'ID', 'value': 1}},
+            mutation_args)
+
+    def test_file_field(self):
+        data_type = {
+            'fields': [
+                {'name': 'a', 'type': 'file', 'relationship': ONE_TO_ONE}
+            ]
+        }
+        data_types = [data_type]
+        args = {'a': 'b'}
+
+        mutation_args = self.__mutation_builder.build_data_type_mutation_args(
+            data_type,
+            data_types,
+            args)
+
+        self.assertEqual(
+            {'a': {'type': 'Upload', 'value': 'b'}},
+            mutation_args)
+
+    def test_reverse_relationship_field(self):
+        data_type = {
+            'type': 'data_type_1',
+            'fields': []
+        }
+        related_data_type = {
+            'type': 'data_type_2',
+            'fields': [
+                {'name': 'b', 'reverseName': 'a', 'type': 'data_type_1', 'relationship': MANY_TO_ONE}
+            ]
+        }
+        data_types = [data_type, related_data_type]
+        args = {'a': [1, 2, 3]}
+
+        mutation_args = self.__mutation_builder.build_data_type_mutation_args(
+            data_type,
+            data_types,
+            args)
+
+        self.assertEqual(
+            {'aId': {'type': '[ID!]', 'value': [1, 2, 3]}},
+            mutation_args)

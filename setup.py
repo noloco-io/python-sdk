20a4b072f46c2ea5a1d6be0d68385cd312edaded
Matt Harrison <matt@noloco.io>
2022-03-10 14:04:43 +0000

    Prepares repository to package on PyPI

--- setup.py
+++ setup.py
@@ -4,13 +4,14 @@ from setuptools import (
 
 
 setup(
-    name='Noloco',
+    name='noloco',
     version='0.1.0',
-    description='Noloco Python SDK',
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
+    description='CRUD operations for Noloco Collections',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     author='Noloco',
     author_email='team@noloco.io',
     license='',  # TODO - write LICENSE
     packages=find_packages(exclude=('docs', 'tests'))
+    install_requires=['gql[all]', 'pydash']
 )

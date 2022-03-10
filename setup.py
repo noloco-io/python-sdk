c0bb749225be38f5b6d2333443671dc31443fdd4
Matt Harrison <matt@noloco.io>
2022-03-10 15:54:32 +0000

    chore: adds MIT license (#28)
    * Licenses under MIT license
    
    * Packages license with PyPI release

--- setup.py
+++ setup.py
@@ -11,7 +11,8 @@ setup(
     long_description_content_type='text/markdown',
     author='Noloco',
     author_email='engineering@noloco.io',
-    license='',  # TODO - write LICENSE
+    license='MIT',
     packages=find_packages(exclude=('docs', 'tests')),
+    include_package_data=True,
     install_requires=['gql[all]', 'pydash']
 )
